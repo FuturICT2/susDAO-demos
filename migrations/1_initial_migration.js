e38cd621b1f47fd92e8b1198ca09287a53a191f7
Benjamin Degenhart <b.degenhart.work@gmail.com>
2020-09-26 16:47:44 +0200

    Add DemoContract to deployment script

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
 const Migrations = artifacts.require("Migrations");
+const DemoContract = artifacts.require("DemoContract");
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
+  deployer.deploy(DemoContract);
 };
